e0769dc2a3620526c061971b9023b2e0a21a90fa
juger rash <jugerrash@jugers-Mac.local>
2019-07-19 04:32:01 -0700

    creating the app

---
+++ goalPost-app/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalPost-app
+//
+//  Created by juger rash on 19.07.19.
+//  Copyright © 2019 juger rash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
